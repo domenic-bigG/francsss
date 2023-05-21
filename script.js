f2ee05f453e77ac16ca35dc6a75059462a38fab1
domenic-bigG <122698822+domenic-bigG@users.noreply.github.com>
2023-05-21 20:58:04 +0200

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+/*
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+        document.body.style['overflow'] = 'hidden';
+    } else if (state == 'complete') {
+        setTimeout(function () {
+            document.getElementById('interactive');
+            document.getElementById('load').style.visibility = "hidden";
+            document.querySelector('body').style.visibility = "visible";
+            document.body.style['overflow'] = 'unset';
+        });
+    }
+}
+*/
+
